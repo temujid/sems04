9b5322019594be51dc3ba70aef09e125d2250a2a
Timur <temuchin@ya.ru>
2022-10-03 02:15:21 +0400

    all_changes

---
+++ task_27/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int number = ReadInt("Введите число: ");
+int lenght = NumberLenght(number);
+SumNumbers(number, lenght);
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int NumberLenght(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+void SumNumbers(int n, int lenght)
+{
+    int sum = 0;
+    for (int i = 1; i <= lenght; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+}
---
+++ task_29/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+
+ввод элеменетов через консоль (через запятую. использовать метод string.Split())
+*/
+
+
+Console.Write("Введите длину массива: ");
+int elementCount = int.Parse(Console.ReadLine());
+int [] myArray = new int [elementCount];
+    
+
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        Console.Write("Введите элемент массива: ");
+
+        myArray[i] = int.Parse(Console.ReadLine());
+    }
+
+    for (int i = 0; i < myArray.Length; i++)
+    {
+    }
+    
+{
+int[] array = new int[] { 1, 2, 3 };
+
+Console.WriteLine("[{0}]", string.Join(", ", myArray));
+}
